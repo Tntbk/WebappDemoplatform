f278b24cae9ad7f9cc0ae71baac6485bc76e61a0
nguyen1872 <thanhnguyen187201@gmail.com>
2023-05-21 21:53:57 +0700

    #1 complete webapp flask-donep1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import *
-
+import numpy as np
 import pandas as pd
 import os
 from werkzeug.utils import secure_filename
@@ -9,48 +9,30 @@ storage_account_key = "eIPOMU+Biqu2L8YtHLRDpSLlkAYK268wlC17HEFeNZVXDVrpQAFU3AGhz
 storage_account_name = "sipblob"
 connection_string = "DefaultEndpointsProtocol=https;AccountName=sipblob;AccountKey=eIPOMU+Biqu2L8YtHLRDpSLlkAYK268wlC17HEFeNZVXDVrpQAFU3AGhz5lUDcx4urBXhwNMKD8i+AStpzdDZw==;EndpointSuffix=core.windows.net"
 
-def load_photo():
-    blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
-    try:
-        container_name = 'photos'
-        container_client = blob_service_client.get_container_client(container=container_name)
-        container_client.get_container_properties() # get properties of the container to force exception to be thrown if container does not exist
-    except Exception as e:
-        print(e)
-        print("Creating container...")
-        container_client = blob_service_client.create_container(container_name) # create a container in the storage account if it does not exist
-        
-    blob_items = container_client.list_blobs() # list all the blobs in the container
-
-    img_html = "<div style='display: flex; justify-content: space-between; flex-wrap: wrap;'>"
-
-    for blob in blob_items:
-        blob_client = container_client.get_blob_client(blob=blob.name) # get blob client to interact with the blob and get blob url
-        img_html += "<img src='{}' width='auto' height='200' style='margin: 0.5em 0;'/> ".format(blob_client.url) # get the blob url and append it to the html
-
-    img_html += "</div>"
-    return img_html
 
 def load_file():
-    blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
-    try:
-        container_name = 'siprawdata'
-        container_client = blob_service_client.get_container_client(container=container_name)
-        container_client.get_container_properties() # get properties of the container to force exception to be thrown if container does not exist
-    except Exception as e:
-        print(e)
-        print("Creating container...")
-        container_client = blob_service_client.create_container(container_name) # create a container in the storage account if it does not exist
-        
+    if request.method == 'POST':
+        blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
+        try:
+            container_name = 'siprawdata'
+            container_client = blob_service_client.get_container_client(container=container_name)
+            container_client.get_container_properties() # get properties of the container to force exception to be thrown if container does not exist
+        except Exception as e:
+            print(e)
+            print("Creating container...")
+            container_client = blob_service_client.create_container(container_name) # create a container in the storage account if it does not exist
+            
 
-    blob_items = container_client.list_blobs() # list all the blobs in the container
+        blob_items = container_client.list_blobs() # list all the blobs in the container
 
-    # lọc ra các blob có phần mở rộng là .csv
-    csv_blobs = [b for b in blob_items if b.name.endswith('.csv')]
+        # lọc ra các blob có phần mở rộng là .csv
+        csv_blobs = [b for b in blob_items if b.name.endswith('.csv')]
 
-    # lấy tên của các tệp CSV
-    csv_files = [b.name for b in csv_blobs]
-    return csv_files
+        # lấy tên của các tệp CSV
+        csv_files = [b.name for b in csv_blobs]
+     
+        return csv_files
+    # return jsonify(a)
 def upload_To_BlobStorage(file_path,file_name,container_name):
     try:
         blob_service = BlobServiceClient.from_connection_string(connection_string)
@@ -59,12 +41,14 @@ def upload_To_BlobStorage(file_path,file_name,container_name):
         with open(file_path,"rb") as data:
             blob_client.upload_blob(data)
         print('uploaded'+file_name+"file")
-        alert = '<div style="color: green;">File uploaded successfully</div>'
+        # alert = '<div style="color: green;">File uploaded successfully</div>'
+        alert = 'OK'
         # Xóa file tạm
         
         return alert
     except:
-        alert = '<div style="color: red;">File upload Existed</div>'
+        # alert = '<div style="color: red;">File upload Existed</div>'
+        alert = 'ERR'
         return alert
 #*** Flask configuration
 
@@ -74,75 +58,130 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.getcwd() + '/static/uploads'
 # RESULT_FOLDER = os.getcwd() + '/static/img_result/'
+file_path_raman = ''
+file_path_operation = ''
+alert = ''
 @app.route('/')
 
 def index():
-    img_html = load_photo()
+    
     csv_file = load_file()
-    return render_template('index.html',img_html = Markup(img_html),csv_file= csv_file)
- 
-@app.route('/',  methods=("POST", "GET"))
-def uploadFile():
+    return render_template('index.html')#,csv_file= csv_file)
+# @app.route('/uploadfile',  methods=("POST", "GET"))
+# def uploadFile():
+#     if request.method == 'POST':
+#        # session['uploaded_data_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], data_filename)
+#         uploaded_file_raman = request.files['uploaded-file-raman']
+#         uploaded_file_operation = request.files['uploaded-file-operation']
+        
+#         filename_raman = secure_filename(uploaded_file_raman.filename)
+#         filename_operation = secure_filename(uploaded_file_operation.filename)
+
+#         # Lưu nội dung file upload vào file tạm
+#         uploaded_file_raman.save(os.path.join(UPLOAD_FOLDER, filename_raman))
+#         uploaded_file_operation.save(os.path.join(UPLOAD_FOLDER, filename_operation))
+        
+#         file_path_raman = os.path.join(UPLOAD_FOLDER, filename_raman)
+#         file_path_operation = os.path.join(UPLOAD_FOLDER, filename_operation)
+#         alert = '<div style="color: green;">File uploaded</div>'
+#         # # Gọi hàm upload file tới Blob Storage
+        
+#         # alert = upload_To_BlobStorage(file_path_raman, filename_raman,container_name='siprawdata')
+#         # alert = upload_To_BlobStorage(file_path_operation, filename_operation,container_name='siprawdata')
+
+#         # # os.remove(file_path_raman)
+#         # # os.remove(file_path_operation)
+        
+
+#         # # Render template with active link
+#         # # img_html = load_photo()
+#         # csv_file = load_file()
+     
+#         return render_template('index.html', alert1=Markup(alert))
+#     return render_template('index.html', active_link='#home')
+
+@app.route('/Load',  methods=("POST", "GET"))
+def uploadInfo():
     if request.method == 'POST':
        # session['uploaded_data_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], data_filename)
-        uploaded_file = request.files['uploaded-file']
-        filename = secure_filename(uploaded_file.filename)
+        uploaded_file_raman = request.files['uploaded-file-raman']
+        uploaded_file_operation = request.files['uploaded-file-operation']
+        CusID = request.form['CusID']
+        ProID = request.form['ProID']
+        BatchID = request.form['BatchID']
+        print(CusID,ProID,BatchID)
+        filename_raman = secure_filename(uploaded_file_raman.filename)
+        filename_operation = secure_filename(uploaded_file_operation.filename)
 
-        # Lưu nội dung file upload vào file tạm
-        uploaded_file.save(os.path.join(UPLOAD_FOLDER, filename))
-        file_path = os.path.join(UPLOAD_FOLDER, filename)
-        # Gọi hàm upload file tới Blob Storage
+        #Lưu nội dung file upload vào file tạm
+        uploaded_file_raman.save(os.path.join(UPLOAD_FOLDER, filename_raman))
+        uploaded_file_operation.save(os.path.join(UPLOAD_FOLDER, filename_operation))
         
-        alert = upload_To_BlobStorage(file_path, filename,container_name='siprawdata')
+        file_path_raman = os.path.join(UPLOAD_FOLDER, filename_raman)
+        file_path_operation = os.path.join(UPLOAD_FOLDER, filename_operation)
+        #Gọi hàm upload file tới Blob Storage
+        print(file_path_raman,file_path_operation)
+        # df_raman = pd.read_csv(file_path_raman)
+        df_operation = pd.read_csv(file_path_operation)
+        
+        df_cus_ID = np.full((df_operation.shape[0], 1), CusID) # create a dataframe with 1 column and value = self.cus_ID
+        df_pro_ID = np.full((df_operation.shape[0], 1), ProID)
+        df_batch_ID = np.full((df_operation.shape[0], 1), BatchID)
+        df_operation_full = pd.concat([pd.DataFrame(df_cus_ID, columns=['Cust']), pd.DataFrame(df_pro_ID, columns=['Project_ID']), pd.DataFrame(df_batch_ID, columns=['2-PAT control(PAT_ref:PAT ref)']),
+                            df_operation], axis=1)
+        df_operation_full.to_csv(file_path_operation)
+        # print(df_operation_full.head())
+        alert = upload_To_BlobStorage(file_path_raman, filename_raman,container_name='siprawdata')
+        alert = upload_To_BlobStorage(file_path_operation, filename_operation,container_name='siprawdata')
 
-        os.remove(file_path)
+        os.remove(file_path_raman)
+        os.remove(file_path_operation)
+        
 
         # Render template with active link
-        img_html = load_photo()
+        # img_html = load_photo()
         csv_file = load_file()
+        
 
-        return render_template('index.html', alert1=Markup(alert),img_html = Markup(img_html),csv_file= csv_file)
-    return render_template('index.html', active_link='#home',img_html = Markup(img_html),csv_file= csv_file)
-
-# định nghĩa một URL để tải xuống các tệp CSV
-# @app.route('/download/<filename>')
-# def download(filename):
-#     blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
-#     container_name = 'test1'
-#     container_client = blob_service_client.get_container_client(container=container_name)
-#     blob_client = container_client.get_blob_client(filename)
-#     file_content = blob_client.download_blob().content_as_text()
-#     print('helooooo')
-#     return Response(
-#         file_content,
-#         mimetype='text/csv',
-#         headers={
-#             "Content-disposition":
-#             f"attachment; filename={filename}"
-#         })
-
-@app.route('/uploadphoto',  methods=("POST", "GET"))
-def uploadPhoto():
+        return jsonify(alert)
+    # return render_template('index.html', active_link='#load-link',csv_file= csv_file,load_link_clicked=True)
+@app.route('/Login', methods=("POST", "GET"))
+def Login():
     if request.method == 'POST':
-       # session['uploaded_data_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], data_filename)
-        uploaded_file = request.files['photos']
-        filename = secure_filename(uploaded_file.filename)
+        user = request.form['user']
+        passw = request.form['pass']
+        print(user,passw)
+        if user == 'admin' and passw == 'admin':
+            loginalerts = 'correct'
+        else:
+            loginalerts = 'e'
+        return jsonify(loginalerts)
+    return jsonify(loginalerts)
 
-        # Lưu nội dung file upload vào file tạm
-        uploaded_file.save(os.path.join(UPLOAD_FOLDER, filename))
-        file_path = os.path.join(UPLOAD_FOLDER, filename)
-        # Gọi hàm upload file tới Blob Storage
+@app.route('/LoadFile',  methods=("POST", "GET"))
+def load_file():
+    if request.method == 'POST':
+        blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
+        try:
+            container_name = 'siprawdata'
+            container_client = blob_service_client.get_container_client(container=container_name)
+            container_client.get_container_properties() # get properties of the container to force exception to be thrown if container does not exist
+        except Exception as e:
+            print(e)
+            print("Creating container...")
+            container_client = blob_service_client.create_container(container_name) # create a container in the storage account if it does not exist
+            
+
+        blob_items = container_client.list_blobs() # list all the blobs in the container
+
+        # lọc ra các blob có phần mở rộng là .csv
+        csv_blobs = [b for b in blob_items if b.name.endswith('.csv')]
+
+        # lấy tên của các tệp CSV
+        csv_files = [b.name for b in csv_blobs]
         
-        alert = upload_To_BlobStorage(file_path, filename,container_name='photos')
+        return jsonify(csv_files)
+    # return jsonify(a)
 
-        # Xóa file tạm
-        os.remove(file_path)
-        img_html = load_photo()
-        csv_file = load_file()
-
-        # Render template with active link
-        print(img_html)
-        return render_template('index.html', active_link=Markup('home'), alert2=Markup(alert),img_html = Markup(img_html),csv_file= csv_file)
-    # return render_template('index.html', active_link='#home',img_html = Markup(img_html))
 if __name__=='__main__':
     app.run(host="0.0.0.0",port = 80)
--- static/script.js
+++ static/script.js
@@ -1,13 +1,15 @@
 // Lấy các phần tử cần thao tác
 const homeLink = document.getElementById("home-link");
-const dashboardLink = document.getElementById("dashboard-link");
-const mlappLink = document.getElementById("mlapp-link");
-const photoLink = document.getElementById("photo-link");
+const dashboardLink = document.getElementById("load-link");
+const mlappLink = document.getElementById("dashboard-link");
+const photoLink = document.getElementById("storage-link");
+const submitlink = document.getElementById("submit-link");
+
 
 const homeSection = document.getElementById("home");
-const dashboardSection = document.getElementById("dashboard");
-const mlappSection = document.getElementById("mlapp");
-const photoSection = document.getElementById("photo");
+const dashboardSection = document.getElementById("load");
+const mlappSection = document.getElementById("dashboard");
+const photoSection = document.getElementById("storage");
 
 
 // Ẩn tất cả các phần nội dung trừ phần Home
@@ -16,7 +18,192 @@ mlappSection.style.display = "none";
 photoSection.style.display = "none";
 
 
-// Thêm sự kiện click cho các link
+// // Thêm sự kiện click cho các link
+// homeLink.addEventListener("click", function(e) {
+//   e.preventDefault(); // Ngăn chặn trình duyệt đi đến URL trong href
+//   homeSection.style.display = "block";
+//   dashboardSection.style.display = "none";
+//   mlappSection.style.display = "none";
+//   photoSection.style.display = "none";
+
+// });
+
+// dashboardLink.addEventListener("click", function(e) {
+//   e.preventDefault(); // Ngăn chặn trình duyệt đi đến URL trong href
+//   homeSection.style.display = "none";
+//   dashboardSection.style.display = "block";
+//   mlappSection.style.display = "none";
+//   photoSection.style.display = "none";
+
+// });
+
+// mlappLink.addEventListener("click", function(e) {
+//   e.preventDefault(); // Ngăn chặn trình duyệt đi đến URL trong href
+//   homeSection.style.display = "none";
+//   dashboardSection.style.display = "none";
+//   mlappSection.style.display = "block";
+//   photoSection.style.display = "none";
+
+// });
+// photoLink.addEventListener("click", function(e) {
+//     e.preventDefault(); // Ngăn chặn trình duyệt đi đến URL trong href
+//     homeSection.style.display = "none";
+//     dashboardSection.style.display = "none";
+//     photoSection.style.display = "block";
+//     mlappSection.style.display = "none";
+    
+  
+//   });
+
+// // Lấy tham chiếu đến form
+// const form = document.getElementById('load-form');
+
+// // Thêm sự kiện submit cho form
+// form.addEventListener('submit', function(e) {
+//   e.preventDefault(); // Ngăn chặn gửi yêu cầu mặc định
+
+//   // Thực hiện xử lý dữ liệu form
+//   // Ví dụ: Kiểm tra dữ liệu hợp lệ, tạo đối tượng FormData, vv.
+
+//   // Tạo đối tượng FormData từ form
+//   const formData = new FormData(form);
+
+//   // Gửi yêu cầu AJAX
+//   fetch('/Load', {
+//     method: 'POST',
+//     body: formData
+//   })
+//   .then(function(response) {
+//     // Xử lý kết quả trả về từ Flask (response)
+//     // Ví dụ: Hiển thị thông báo thành công
+//     alert('Yêu cầu đã được gửi thành công!');
+//     // Lấy tham chiếu đến phần tử <p>
+//     const alertMessage = document.getElementById('alert-message');
+
+//     // Cập nhật nội dung của phần tử <p> bằng giá trị của biến alert1
+//     alertMessage.innerHTML = '<div style="color: red;">File upload Existed</div>';
+
+//     // Tùy chỉnh các hành động sau khi gửi yêu cầu thành công
+//     // Ví dụ: Cập nhật giao diện, chuyển hướng trang, vv.
+//   })
+//   .catch(function(error) {
+//     // Xử lý lỗi trong quá trình gửi yêu cầu
+//     // Ví dụ: Hiển thị thông báo lỗi
+//     alert('Đã xảy ra lỗi khi gửi yêu cầu!');
+//   });
+// });
+
+// Lấy tham chiếu đến form và phần tử <p> để hiển thị phản hồi
+const form = document.getElementById('load-form');
+const responseMessage = document.getElementById('alert-message');
+const cusIdInput = document.querySelector('input[name="CusID"]');
+const proIdInput = document.querySelector('input[name="ProID"]');
+const batchIdInput = document.querySelector('input[name="BatchID"]');
+// Đặt sự kiện submit cho form
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); // Ngăn chặn hành vi mặc định khi submit form
+
+  // Tạo đối tượng FormData từ form
+  const formData = new FormData(form);
+  // Kiểm tra các trường input
+  if (cusIdInput.value.trim() === "" || proIdInput.value.trim() === "" || batchIdInput.value.trim() === "") {
+    responseMessage.innerHTML = '<div style="color: red;">Vui lòng điền đầy đủ thông tin</div>';
+    return; // Dừng tiếp tục xử lý và hiển thị thông báo lỗi
+  }
+  // Xử lý sự kiện input trên trường BatchID
+  
+  const inputValue = batchIdInput.value.trim();
+
+  // Kiểm tra giá trị nhập vào bằng biểu thức chính quy chỉ cho phép số
+  if (!/^\d*$/.test(inputValue)) {
+    responseMessage.innerHTML = '<div style="color: red;">BatchID chỉ nhận giá trị số</div>'; // Xóa các ký tự không phải số
+    return
+  }
+ 
+  // Gửi yêu cầu POST đến máy chủ Flask
+  fetch('/Load', {
+    method: 'POST',
+    body: formData
+  })
+  .then(function(response) {
+    // Xử lý phản hồi từ máy chủ Flask
+    if (response.ok) {
+      return response.text();
+      // response.text().then(function (data) {
+      //   alert(data)
+
+      //   // var alertMessage = '<div style="color: red;">' + data + '</div>';
+      // //  responseMessage.innerHTML = alertMessage;
+      // }); // Trả về nội dung phản hồi dưới dạng văn bản
+    } else {
+      throw new Error('Error: ' + response.status);
+    }
+  })
+  .then(function(data) {
+    // Xử lý dữ liệu phản hồi
+
+    // alert(data)
+    
+    if (data.match("OK")) {
+      // alert('cccc')
+
+      responseMessage.innerHTML = '<div style="color: green;">File uploaded successfully</div>'; // Hiển thị phản hồi trong phần tử <p>
+    // Thêm sự kiện click cho các link
+  } else {
+  var alertMessage = '<div style="color: red;">File upload Existed</div>';
+  responseMessage.innerHTML = alertMessage;
+  }
+})
+  .catch(function(error) {
+    console.log(error);
+    alert('An error occurred during the request.');
+  });
+});
+
+
+const loginMessage = document.getElementById('login-message');
+const loginMain = document.getElementById("login-main");
+
+const userinput = document.querySelector('input[name="user"]');
+const passinput = document.querySelector('input[name="pass"]');
+const loginform = document.getElementById('login-form');
+loginform.addEventListener('submit', function(e) {
+  e.preventDefault(); // Ngăn chặn hành vi mặc định khi submit form
+
+  // Tạo đối tượng FormData từ form
+  const formData = new FormData(loginform);
+  // Kiểm tra các trường input
+  if (userinput.value.trim() === "" || passinput.value.trim() === "" ) {
+    loginMessage.innerHTML = '<div style="color: red;">Vui lòng điền đầy đủ thông tin</div>';
+    return; // Dừng tiếp tục xử lý và hiển thị thông báo lỗi
+  }
+  
+  // Gửi yêu cầu POST đến máy chủ Flask
+  fetch('/Login', {
+    method: 'POST',
+    body: formData
+  })
+  .then(function(response) {
+    // Xử lý phản hồi từ máy chủ Flask
+    if (response.ok) {
+      return response.text();
+      // response.text().then(function (data) {
+      //   alert(data)
+
+      //   // var alertMessage = '<div style="color: red;">' + data + '</div>';
+      // //  responseMessage.innerHTML = alertMessage;
+      // }); // Trả về nội dung phản hồi dưới dạng văn bản
+    } else {
+      throw new Error('Error: ' + response.status);
+    }
+  })
+  .then(function(data) {
+    // alert(data.match("correct"))
+    if (data.match("correct")) {
+      // alert('cccc')
+
+      loginMain.innerHTML = '<div style="color: green;"><h2>Welcome to Demo Digital Twin platform</h2></div>'; // Hiển thị phản hồi trong phần tử <p>
+    // Thêm sự kiện click cho các link
 homeLink.addEventListener("click", function(e) {
   e.preventDefault(); // Ngăn chặn trình duyệt đi đến URL trong href
   homeSection.style.display = "block";
@@ -52,4 +239,70 @@ photoLink.addEventListener("click", function(e) {
     
   
   });
-// document.getElementById("home").innerHTML = "Hello, world!";
+    } else {
+      var alertMessage = '<div style="color: red;">User name or password incorrect.</div>';
+      loginMessage.innerHTML = alertMessage;
+    }
+  })
+  .catch(function(error) {
+    console.log(error);
+    alert('An error occurred during the request.');
+  });
+});
+
+
+
+
+
+// Lấy tham chiếu đến form và phần tử <p> để hiển thị phản hồi
+const formloadfile = document.getElementById('loadfile-form');
+const listfile = document.getElementById('file-list');
+
+// Đặt sự kiện submit cho form
+formloadfile.addEventListener('submit', function(e) {
+  e.preventDefault(); // Ngăn chặn hành vi mặc định khi submit form
+
+  // Tạo đối tượng FormData từ form
+  const formfile = new FormData(formloadfile);
+  
+  // Gửi yêu cầu POST đến máy chủ Flask
+  fetch('/LoadFile', {
+    method: 'POST',
+    body: formfile
+  })
+  .then(function(response) {
+    // Xử lý phản hồi từ máy chủ Flask
+    if (response.ok) {
+      return response.json();
+      // response.text().then(function (data) {
+      //   alert(data)
+
+      //   // var alertMessage = '<div style="color: red;">' + data + '</div>';
+      // //  responseMessage.innerHTML = alertMessage;
+      // }); // Trả về nội dung phản hồi dưới dạng văn bản
+    } else {
+      throw new Error('Error: ' + response.status);
+    }
+  })
+  .then(function(data) {
+    // Xử lý dữ liệu phản hồi
+
+    // alert(data)
+    // // const fileListElement = document.getElementById('file-list');
+    // for (let i = 0; i < data.length; i++) {
+    //   const filename = data[i];
+    //   alert(filename);
+    // }
+//     // / Xử lý phản hồi từ Flask
+    const fileList = document.getElementById('file-list');
+    data.forEach(filename => {
+      const listItem = document.createElement('li');
+      listItem.textContent = filename;
+      fileList.appendChild(listItem);
+})
+  })
+  .catch(function(error) {
+    console.log(error);
+    alert('An error occurred during the request.');
+  });
+});
